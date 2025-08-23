b366e86976f8ea9fe6a716bb72d63bafb22c3d9f
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-08-23 17:27:36 +0530

    a

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 export const appName = import.meta.env.VITE_APP_NAME || "Frame";
 export const backendUrl =
-  import.meta.env.VITE_BACKEND_URL || "https://frame.3.7.237.251.sslip.io/api/";
+  import.meta.env.VITE_BACKEND_URL || "https://skincarevoices.com/api/";
 export const backendStaticUrl =
-  import.meta.env.VITE_BACKEND_STATIC_URL ||
-  "https://frame.3.7.237.251.sslip.io";
+  import.meta.env.VITE_BACKEND_STATIC_URL || "https://skincarevoices.com";
 export const allowRegistration =
   import.meta.env.VITE_ALLOW_REGISTRATION === "true" || false;
 export const frontendUrl =
-  import.meta.env.VITE_FRONTEND_URL || "https://frame.3.7.237.251.sslip.io";
+  import.meta.env.VITE_FRONTEND_URL || "https://skincarevoices.com";
